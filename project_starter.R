138cfd365529d5c6a5f935c6ea5ac31f301c668a
jacob-mattia <140334987+jacob-mattia@users.noreply.github.com>
2023-08-18 17:39:02 -0500

    Update project_starter.R
    added biocmanager

--- project_starter.R
+++ project_starter.R
@@ -11,6 +11,8 @@
 ####BASE INSTALLS####
 #Installs and runs libraries necessary for basic formatting and data manipulation
 
+if (!require("BiocManager", character.only = TRUE)){renv::install("BiocManager")}
+BiocManager::install()
 if (!require("conflicted", character.only = TRUE)){renv::install("conflicted", dependencies = TRUE)}
 library(conflicted)
 if (!require("tidyverse", character.only = TRUE)){renv::install("tidyverse", dependencies = TRUE)}
