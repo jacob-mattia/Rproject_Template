4e2848f7ad571c0e1a3d77d5a59b8b29f14c3c5c
jacob-mattia <140334987+jacob-mattia@users.noreply.github.com>
2023-08-21 11:24:17 -0500

    Update project_starter.R
    updated renv_initialized object

--- project_starter.R
+++ project_starter.R
@@ -13,7 +13,8 @@
 if (!require("renv", character.only = TRUE)){install.packages("renv", dependencies = TRUE)}
 library(renv)
 if (!file.exists("renv_initialized.RData")){
-  save("TRUE", file = "renv_initialized.RData")
+  renv_initialized <- TRUE
+  save(renv_initialized, file = "renv_initialized.RData")
   renv::init()
 }
 if (!require("conflicted", character.only = TRUE)){renv::install("conflicted", dependencies = TRUE)}
