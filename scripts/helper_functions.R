7eb1db6baa5f95f18563ca200787a198a63205ef
jacob-mattia <140334987+jacob-mattia@users.noreply.github.com>
2023-08-28 10:03:52 -0500

    Update helper_functions.R
    Added initial comments

---
+++ scripts/helper_functions.R
@@ -0,0 +1,9 @@
+####META DATA####
+#helper_functions
+#v0.0.0
+#Original author:
+#Summary: common functions can be stored here to de-clutter main analysis code
+#Version change:
+#NOTE:
+
+####HELPER FUNCTION SECTIONS####
