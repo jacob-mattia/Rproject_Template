810d8ab5015afebbeba049ddfd54155b6a2895ec
jacob-mattia <140334987+jacob-mattia@users.noreply.github.com>
2023-08-28 09:51:30 -0500

    Update 1_renv_activation.R
    updated comment format

--- starter_scripts/1_renv_activation.R
+++ starter_scripts/1_renv_activation.R
@@ -3,8 +3,8 @@
 ####META DATA####
 #renv_activation
 #v0.0.1
-#Jacob Mattia
-#Activates renv environment. Run at the start of each session.
+#Original author: Jacob Mattia
+#Summary: Activates renv environment. Run at the start of each session.
 #Version change: initial version
 #NOTE: Only need to run once
 
