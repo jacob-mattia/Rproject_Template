5c79b231f37969436b3f1b28bdcdd7e26ebc2663
jacob-mattia <140334987+jacob-mattia@users.noreply.github.com>
2023-08-28 10:02:09 -0500

    Update analysis.R
    Added initial commenting

---
+++ scripts/analysis.R
@@ -0,0 +1,30 @@
+#!/usr/bin/Rscript
+
+####META DATA####
+#analysis
+#v0.0.0
+#Original author:
+#Summary:
+#Version change:
+#NOTE:
+
+####DATA TYPE(S)####
+#Data Type 1:
+
+####OUTPUT TYPE(S)####
+#Output Type 1:
+
+####WORKFLOW####
+#1.
+#2.
+#3.
+
+####LIBRARY INSTALLS####
+renv::restore() #Loads in current renv.lock
+#Add libraries here
+
+renv::snapshot()
+
+####INPUT FILES (should all be in /data)####
+
+####ANALYSIS SECTIONS####
